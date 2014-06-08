3d0d21c2086c38f934d5b9982ce1342855dc1c98
Matt Murfitt <murfitt@gmail.com>
2014-06-08 00:04:05 -0700

    Sped up mandelbrot creation. Added node.js performance numbers to test_timing.py

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,6 +1,7 @@
 #mandelbrot.py
 #creates a mandelbrot for the given coordinates
 import numpy as np
+from matplotlib import pyplot
 
 def mand(iters, tl, br, xpx, ypx=None):
     ypx = ypx or xpx
@@ -11,19 +12,33 @@ def mand(iters, tl, br, xpx, ypx=None):
     for it in xrange(iters):
         m *= m
         m += c
-        ret += np.abs(m) < 2
+        ret += np.real(m*m.conjugate()) < 4
     #want inf. iterations to be a single color, not based on # of iterations
     ret += -(iters+1)*(np.abs(m) < 2)
     return ret
 
+def mand2(iters, tl, br, xpx, ypx=None):
+    ypx = ypx or xpx
+    im, re = np.mgrid[tl[0]: br[0]: xpx*1j,
+                      tl[1]: br[1]: ypx*1j]
+    ore, oim = re.copy(), im.copy()
+    ret=np.ones(ore.shape, dtype=np.int32)
+    for it in xrange(iters):
+        r2 = re**2
+        i2 = im**2
+        re, im = ore + r2 - i2, oim + 2 * re * im 
+        ret += (r2 + i2) < 4  # faster for me than ret[(r2 + i2)< 4] += 1
+    #want inf. iterations to be a single color, not based on # of iterations
+    ret[(r2 + i2) < 4] = 0
+    return ret
+
+
 def mand_and_write(file, iters, tl, br, xpx, ypx=None):
-    from matplotlib import pyplot
-    m=mand(iters, tl, br, xpx, ypx)
+    m=mand2(iters, tl, br, xpx, ypx)
     pyplot.imsave(file, m, cmap='spectral', vmin=0, vmax=255)
     
 if __name__=="__main__":
-    from matplotlib import pyplot
-    m=mand(16, (-2, -2), (2, 2), 256)
+    m=mand2(16, (-2, -2), (2, 2), 256)
     pyplot.imshow(m, cmap='spectral')	
     pyplot.show() 
 
--- test_timing.py
+++ test_timing.py
@@ -6,7 +6,7 @@ Times how long 100 requests to localhost take!
 import timeit
 import urllib
 
-def test_url(times = [-2.001+0.01*x for x in range(100)]):
+def test_url(times = [-2.001+0.01*x for x in range(200)]):
     for t in times:
         # using 'localhost' instaed of 127.0.0.1 takes ~1s
         # per request!
@@ -21,8 +21,21 @@ print timeit.timeit(stmt=test_url, number=1)
 in s, first time (cached)
 go: 3.0 1.63
 python: 21.9 0.7
+trying again 2013-11-10 (MBP, plugged in, VM, 100images):
+python: 10.2 (0.43)
+go: 1.4 (0.85)
+
+As above 200 images:
+Go: 2.55 (1.55)
+Python: 20.4 (0.84)
+Python: mand2 13.4 (0.84)  # we don't use complex data and cache re and im squared
+node.js 4.6 (0.55 - caching already pngd data)
+
 Python caches the png, whereas go caches the raw data and has to
 compress each time.
+
+NB Python with sending blank ramps into png takes 4.5s (ie the png encoding is
+slower than both go and node.js total time)
 
 Still not sure what takes so long with the Python code. cProfile says
 select.select takes all the time, but I think that could be waiting for 
